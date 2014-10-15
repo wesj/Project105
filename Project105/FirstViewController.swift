bc4d1228d472fc234f500040a994d1bcfa6d62a5
Wes Johnston <we.j@live.com>
2014-10-14 23:12:12 -0700

    Initial commit
    Signed-off-by: Wes Johnston <we.j@live.com>

---
+++ Project105/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Project105
+//
+//  Created by Wes Johnston on 10/14/14.
+//  Copyright (c) 2014 Wes Johnston. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
